df0bd42e5a613cbaa7fd6e3220b7746f195c725f
tapthruyeyuyan <102268434+tapthruyeyuyan@users.noreply.github.com>
2022-10-24 17:00:18 +0800

    10-24  17.00

--- src/store/storeInfoSlice.js
+++ src/store/storeInfoSlice.js
@@ -2,19 +2,35 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   storeInfo: {},
+  orderList: {
+    orderItems: [],
+  },
 };
 
 export const storeInfo = createSlice({
   name: "counter",
   initialState,
   reducers: {
+    /**
+     * @description: 获取商户信息
+     * @return {*}
+     */
     getStoreInfo: (state, action) => {
       state.storeInfo = action.payload;
+    },
+    /**
+     * @description: 添加目录
+     * @return {*}
+     */
+    addOrderItems: (state, action) => {
+      // console.log(action.payload);
+      // console.log(state.orderList.orderItems);
+      state.orderList.orderItems.push(action.payload);
     },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { getStoreInfo } = storeInfo.actions;
+export const { getStoreInfo, addOrderItems } = storeInfo.actions;
 
 export default storeInfo.reducer;
--- view/Order/OrderPage.jsx
+++ view/Order/OrderPage.jsx
@@ -1,5 +1,5 @@
 import { Button, Popover, Dropdown, Menu, Input, Modal, Calendar } from "antd";
-import { ExclamationCircleOutlined } from "@ant-design/icons";
+import { ExclamationCircleOutlined, MinusCircleOutlined, PlusCircleOutlined } from "@ant-design/icons";
 import React, { useEffect, useState } from "react";
 import { dish } from "../../Mock";
 import {
@@ -28,26 +28,549 @@ import ChangePrice from "../../component/Tax/ChangePrice";
 import { useNavigate } from "react-router-dom";
 import Keyboard from "../../component/Keyboard";
 import Notepad from "../../component/Notepad";
+import { useDispatch, useSelector } from "react-redux";
+import ItemOptions from "./commponent/ItemOptions";
+import { addOrderItems } from "../../src/store/storeInfoSlice";
 
 const { confirm } = Modal;
 
 const OrderPage = () => {
-  useEffect(() => {
-    console.log(dish);
-  }, [dish]);
-
   const [dishs, SetDishs] = useState({});
 
   const [checkText, setCheckText] = useState("");
 
-  useEffect(() => {
-    console.log(checkText);
-  }, [checkText]);
-
   const navigate = useNavigate();
+
+  const dispatch = useDispatch();
+
+  const orderList = useSelector((state) => state.orderList);
+
+  const windowWidth = document.body.clientWidth;
+
+  const [dishList, setDishList] = useState([]);
+
+  /**
+   * @description: 分页
+   * @param {*} dishListNumber 最多多少个
+   * @param {*} optionsListNumber 最多多少个减少一个
+   * @return {*}
+   */
+  const paging = (dishListNumber, optionsListNumber) => {
+    let temp = [];
+
+    if (dish.length > dishListNumber) {
+      for (let i = 0; i < dish.length; ) {
+        temp.push(dish.slice(i, (i += optionsListNumber)));
+      }
+
+      for (let i = 0; i < temp.length; i++) {
+        for (let j = 0; j < temp[i].length; j++) {
+          if (temp[i][j].items.length > dishListNumber) {
+            let temp_ = [];
+            for (let o = 0; o < temp[i][j].items.length; ) {
+              temp_.push(temp[i][j].items.slice(o, (o += optionsListNumber)));
+            }
+            // console.log("temp_", temp_);
+            temp[i][j].items = temp_;
+          }
+        }
+      }
+    }
+    setDishList(JSON.parse(JSON.stringify(temp)));
+  };
+
+  useEffect(() => {
+    if (windowWidth < 1745 && windowWidth >= 1607) {
+      paging(30, 29);
+    } else if (windowWidth < 1606 && windowWidth >= 1468) {
+      paging(27, 26);
+    } else if (windowWidth <= 1467 && windowWidth >= 1329) {
+      paging(24, 23);
+    } else if (windowWidth <= 1328 && windowWidth >= 1191) {
+      paging(21, 20);
+    } else if (windowWidth <= 1190 && windowWidth >= 1069) {
+      paging(18, 17);
+    } else if (windowWidth <= 1068 && windowWidth >= 965) {
+      paging(15, 14);
+    } else if (windowWidth <= 964 && windowWidth >= 861) {
+      paging(12, 11);
+    } else if (windowWidth <= 860 && windowWidth >= 750) {
+      paging(9, 8);
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log(dishList);
+  }, [dishList]);
+
+  /**
+   * @description: 弹窗显示
+   * @return {*}
+   */
+  const [dishShow, setDishShow] = useState(false);
+
+  // 餐厅模拟数据
+  const mock = {
+    restaurantInfoId: 3,
+    active: true,
+    id: 1116206,
+    itemId: "0",
+    name: "Create Your Own Combination ",
+    cnName: "Create Your Own Combination ",
+    description: "Choose any two items.",
+    price: 22.95,
+    mPrice: null,
+    selectItems: null,
+    spicy: false,
+    raw: false,
+    taxExempt: false,
+    displayOrder: 0,
+    itemImage: "https://etapthru.com/img/itemImage.png",
+    encodedItemImage: null,
+    printerName: "[]",
+    printable: true,
+    categoryOptionAvailable: true,
+    itemOptionGroups: [
+      {
+        restaurantInfoId: 3,
+        id: 65176,
+        name: "Choose 2 Items",
+        value: "New Modifier",
+        numOfChoice: 2,
+        required: true,
+        multiQtyAllowed: true,
+        itemOptions: [
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252945,
+            name: "Vegetable",
+            cnName: "Vegetable",
+            price: 0,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252946,
+            name: " Chicken",
+            cnName: " Chicken",
+            price: 0,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252947,
+            name: "Shrimp",
+            cnName: "Shrimp",
+            price: 0,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252948,
+            name: "Red Snapper",
+            cnName: "Red Snapper",
+            price: 1.5,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252949,
+            name: "Calamari",
+            cnName: "Calamari",
+            price: 0,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252950,
+            name: " Salmon",
+            cnName: " Salmon",
+            price: 0,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252951,
+            name: "Scallop",
+            cnName: "Scallop",
+            price: 2,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252952,
+            name: "Lobster (6 oz) ",
+            cnName: "Lobster (6 oz) ",
+            price: 5,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252953,
+            name: "New York Steak (Rare)",
+            cnName: "New York Steak (Rare)",
+            price: 2.5,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252954,
+            name: "New York Steak (Medium Rare)",
+            cnName: "New York Steak (Medium Rare)",
+            price: 2.5,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252955,
+            name: "New York Steak (Medium)",
+            cnName: "New York Steak (Medium)",
+            price: 2.5,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252956,
+            name: "New York Steak (Medium Well)",
+            cnName: "New York Steak (Medium Well)",
+            price: 2.5,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252957,
+            name: "New York Steak (Well Done)",
+            cnName: "New York Steak (Well Done)",
+            price: 2.5,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252958,
+            name: "Filet Mignon (Rare)",
+            cnName: "Filet Mignon (Rare)",
+            price: 4,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252959,
+            name: "Filet Mignon (Medium Rare)",
+            cnName: "Filet Mignon (Medium Rare)",
+            price: 4,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252960,
+            name: "Filet Mignon (Medium)",
+            cnName: "Filet Mignon (Medium)",
+            price: 4,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252961,
+            name: "Filet Mignon (Medium Well)",
+            cnName: "Filet Mignon (Medium Well)",
+            price: 4,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+          {
+            restaurantInfoId: 3,
+            active: true,
+            id: 252962,
+            name: "Filet Mignon (Well Done)",
+            cnName: "Filet Mignon (Well Done)",
+            price: 4,
+            optionType: null,
+            deleted: false,
+            employee: null,
+            default: false,
+          },
+        ],
+        active: true,
+      },
+    ],
+    optionsList: [
+      {
+        restaurantInfoId: 3,
+        id: null,
+        name: "Choose 2 Items",
+        value: "New Modifier",
+        numOfChoice: 2,
+        required: true,
+        multiQtyAllowed: true,
+        groupId: "ItemOptionGroup_65176",
+        optionLocation: "ITEM",
+        options: [
+          {
+            id: "ItemOptionGroup_252945",
+            name: "Vegetable",
+            cnName: "Vegetable",
+            price: 0,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252946",
+            name: " Chicken",
+            cnName: " Chicken",
+            price: 0,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252947",
+            name: "Shrimp",
+            cnName: "Shrimp",
+            price: 0,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252948",
+            name: "Red Snapper",
+            cnName: "Red Snapper",
+            price: 1.5,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252949",
+            name: "Calamari",
+            cnName: "Calamari",
+            price: 0,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252950",
+            name: " Salmon",
+            cnName: " Salmon",
+            price: 0,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252951",
+            name: "Scallop",
+            cnName: "Scallop",
+            price: 2,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252952",
+            name: "Lobster (6 oz) ",
+            cnName: "Lobster (6 oz) ",
+            price: 5,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252953",
+            name: "New York Steak (Rare)",
+            cnName: "New York Steak (Rare)",
+            price: 2.5,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252954",
+            name: "New York Steak (Medium Rare)",
+            cnName: "New York Steak (Medium Rare)",
+            price: 2.5,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252955",
+            name: "New York Steak (Medium)",
+            cnName: "New York Steak (Medium)",
+            price: 2.5,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252956",
+            name: "New York Steak (Medium Well)",
+            cnName: "New York Steak (Medium Well)",
+            price: 2.5,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252957",
+            name: "New York Steak (Well Done)",
+            cnName: "New York Steak (Well Done)",
+            price: 2.5,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252958",
+            name: "Filet Mignon (Rare)",
+            cnName: "Filet Mignon (Rare)",
+            price: 4,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252959",
+            name: "Filet Mignon (Medium Rare)",
+            cnName: "Filet Mignon (Medium Rare)",
+            price: 4,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252960",
+            name: "Filet Mignon (Medium)",
+            cnName: "Filet Mignon (Medium)",
+            price: 4,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252961",
+            name: "Filet Mignon (Medium Well)",
+            cnName: "Filet Mignon (Medium Well)",
+            price: 4,
+            active: true,
+          },
+          {
+            id: "ItemOptionGroup_252962",
+            name: "Filet Mignon (Well Done)",
+            cnName: "Filet Mignon (Well Done)",
+            price: 4,
+            active: true,
+          },
+        ],
+        active: true,
+      },
+    ],
+    startTime: null,
+    endTime: null,
+    availability: "ALL",
+    deleted: false,
+    categoryCnName: "自选套餐",
+    employee: null,
+    categoryId: 125060,
+    categoryName: "Create Your Own Dinner Combination",
+  };
+
+  // 添加菜品列表
+  const [orderListTemp, setOrderListTemp] = useState([]);
+
+  // useEffect(() => {
+  //   console.log(orderListTemp);
+  // }, [orderListTemp]);
+
+  /**
+   * @description: 提交信息
+   * @return {*}
+   */
+  const [orderInfo, setOrderInfo] = useState({
+    restaurantInfoId: mock.restaurantInfoId,
+    quantity: 1,
+    name: mock.name,
+    cnName: mock.cnName,
+    price: mock.price,
+    itemId: mock.id,
+    orderItemsOptions: [],
+    categoryId: mock.categoryId,
+    categoryName: mock.categoryName,
+    categoryCnName: mock.categoryCnName,
+    specialInstructions: null,
+    printerName: "[]",
+    printable: true,
+    mOrderItem: {
+      price: 0,
+    },
+    taxExempt: false,
+  });
+
+  /**
+   * @description: 点击ok
+   * @return {*}
+   */
+  const touchOK = () => {
+    dispatch(addOrderItems({ ...orderInfo, orderItemsOptions: orderListTemp }));
+    setDishShow(false);
+  };
+
+  /**
+   * @description: 点击关闭
+   * @return {*}
+   */
+  const touchCancel = () => {
+    setOrderListTemp([]);
+    setDishShow(false);
+  };
 
   return (
     <>
+      <DishList
+        mock={mock}
+        dishShow={dishShow}
+        touchOK={touchOK}
+        touchCancel={touchCancel}
+        orderListTemp={orderListTemp}
+        setOrderListTemp={setOrderListTemp}
+      />
       <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
         <div className='order-header'>
           <div className='order-header-title'>Language</div>
@@ -58,7 +581,7 @@ const OrderPage = () => {
             <div className='order-box1-content'>
               <div className='order-content-title'>Category</div>
               <div className='order-content-category'>
-                {/* {dish.map((item, index) => (
+                {dish.map((item, index) => (
                   <Button
                     type='text'
                     className='order-content-category-btn'
@@ -66,7 +589,7 @@ const OrderPage = () => {
                     onClick={() => SetDishs(JSON.parse(JSON.stringify(item)))}>
                     {item.name}
                   </Button>
-                ))} */}
+                ))}
               </div>
             </div>
             <div className='order-box1-content'>
@@ -74,7 +597,13 @@ const OrderPage = () => {
               <div className='order-content-category'>
                 {JSON.stringify(dishs) !== "{}" &&
                   dishs.items.map((item, index) => (
-                    <Button type='text' className='order-content-category-btn' key={index.toString()}>
+                    <Button
+                      type='text'
+                      className='order-content-category-btn'
+                      key={index.toString()}
+                      onClick={() => {
+                        setDishShow(true);
+                      }}>
                       {item.name}
                     </Button>
                   ))}
@@ -475,6 +1004,63 @@ const SplitCheck = () => {
       console.log("Cancel");
     },
   });
+};
+
+const DishList = ({ dishShow, touchOK, touchCancel, mock, orderListTemp, setOrderListTemp }) => {
+  // 选择次数
+  const [selectNumber, setSelectNumber] = useState(0);
+
+  return (
+    <Modal
+      title={
+        <div>
+          <h2>{mock.name}</h2>
+          <div style={{ color: "rgb(118, 118, 118)" }}>{mock.description}</div>
+        </div>
+      }
+      open={dishShow}
+      onCancel={touchCancel}
+      onOk={touchOK}
+      centered
+      footer={[
+        <div key='dish' style={{ display: "flex", alignItems: "center", marginRight: 10 }}>
+          <MinusCircleOutlined style={{ fontSize: 20 }} />
+          <div className='dishList-number'>1</div>
+          <PlusCircleOutlined style={{ fontSize: 20 }} />
+        </div>,
+        <Button key='OK' type='primary' onClick={touchOK}>
+          OK
+        </Button>,
+      ]}>
+      <div className='dishList-header'>
+        <div>
+          <p style={{ fontSize: 16, fontWeight: "bold" }}>Choose {mock.itemOptionGroups[0].numOfChoice} Items</p>
+          <p style={{ fontSize: 14 }}>Select {selectNumber}</p>
+        </div>
+        <div>Required</div>
+      </div>
+      <div>
+        {mock.itemOptionGroups.map((item, index) => (
+          <div key={index.toString()}>
+            {item.itemOptions.map((item_, index_) => (
+              <div key={index_.toString()} className='dishList-list'>
+                <ItemOptions
+                  item={item_}
+                  index={index_}
+                  mock={mock}
+                  orderListTemp={orderListTemp}
+                  setOrderListTemp={setOrderListTemp}
+                  selectNumber={selectNumber}
+                  setSelectNumber={setSelectNumber}
+                  numOfChoice={item.numOfChoice}
+                />
+              </div>
+            ))}
+          </div>
+        ))}
+      </div>
+    </Modal>
+  );
 };
 
 export default OrderPage;
